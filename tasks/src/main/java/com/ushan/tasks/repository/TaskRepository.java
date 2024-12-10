8698f18713a7cec00ad9656ef4758506a56b37a8
@kavinduushan07 <kavinduushan07@gmail.com>
2024-12-10 15:07:45 +0530

    remove test files

--- tasks/src/main/java/com/ushan/tasks/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ushan.tasks.repository;
-
-import com.ushan.tasks.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-
-}
