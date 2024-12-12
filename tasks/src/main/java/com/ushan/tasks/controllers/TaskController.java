600351652556b02b5b3a178b5cbcb2f60e6ca1cf
@kavinduushan07 <kavinduushan07@gmail.com>
2024-12-12 11:09:59 +0530

    implement create Task

--- tasks/src/main/java/com/ushan/tasks/controllers/TaskController.java
+++ tasks/src/main/java/com/ushan/tasks/controllers/TaskController.java
@@ -1,12 +1,10 @@
 package com.ushan.tasks.controllers;
 
 import com.ushan.tasks.domain.dto.TaskDto;
+import com.ushan.tasks.domain.entities.Task;
 import com.ushan.tasks.mappers.TaskMapper;
 import com.ushan.tasks.services.TaskService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
@@ -29,5 +27,17 @@ public class TaskController {
                 .stream()
                 .map(taskMapper::toDto)
                 .toList();
+    }
+
+    @PostMapping
+    public TaskDto createTask(
+            @PathVariable("task_list_id")UUID taskListId,
+            @RequestBody TaskDto taskDto
+    ){
+        Task createdTask = taskService.createTask(
+                taskListId,
+                taskMapper.fromDto(taskDto)
+        );
+        return taskMapper.toDto(createdTask);
     }
 }
