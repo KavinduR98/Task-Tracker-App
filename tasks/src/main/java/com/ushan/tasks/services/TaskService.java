e101be9e2bcc7b9524c62ad9fd06caa43d7f4662
@kavinduushan07 <kavinduushan07@gmail.com>
2024-12-12 12:11:49 +0530

    implement update Task

--- tasks/src/main/java/com/ushan/tasks/services/TaskService.java
+++ tasks/src/main/java/com/ushan/tasks/services/TaskService.java
@@ -10,4 +10,5 @@ public interface TaskService {
     List<Task> listTasks(UUID taskListId);
     Task createTask(UUID taskListId, Task task);
     Optional<Task> getTask(UUID taskListID, UUID taskId);
+    Task updateTask(UUID taskListId, UUID taskId, Task task);
 }
