d3c6a40523841f8c2f631414e3ba8ff86107558e
@kavinduushan07 <kavinduushan07@gmail.com>
2024-12-12 10:47:03 +0530

    implement List Task

---
+++ tasks/src/main/java/com/ushan/tasks/services/TaskService.java
@@ -0,0 +1,10 @@
+package com.ushan.tasks.services;
+
+import com.ushan.tasks.domain.entities.Task;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TaskService {
+    List<Task> listTasks(UUID taskListId);
+}
---
+++ tasks/src/main/java/com/ushan/tasks/services/impl/TaskServiceImpl.java
@@ -0,0 +1,24 @@
+package com.ushan.tasks.services.impl;
+
+import com.ushan.tasks.domain.entities.Task;
+import com.ushan.tasks.repositories.TaskRepository;
+import com.ushan.tasks.services.TaskService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public List<Task> listTasks(UUID taskListId) {
+        return taskRepository.findByTaskListId(taskListId);
+    }
+}
